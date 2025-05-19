58efbe83edca87d70755d0370e53d662b550aa3f
Anton Olguin <antonolguin1@gmail.com>
2025-05-19 00:43:12 -0600

    Add project files.

---
+++ src/Algorithms.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordenamiento_anton.src
+{
+    public static class Algorithms
+    {
+        public static void BubbleSort(List<int> arr)
+        {
+            int size = arr.Count(); 
+            for(int i = size - 1; i > 0; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        //Shaker
+
+        // Insertion
+
+        // Selection
+
+        // Shell
+
+        // Quick Sort
+
+        // Heap Sort
+    }
+}
