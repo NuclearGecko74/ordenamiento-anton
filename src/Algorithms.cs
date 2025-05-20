5f69b421f77bb0e07ff02dee7816a94477040f59
Anton Olguin <antonolguin1@gmail.com>
2025-05-20 00:29:38 -0600

    Sorts

--- src/Algorithms.cs
+++ src/Algorithms.cs
@@ -26,15 +26,101 @@ namespace ordenamiento_anton.src
         }
 
         //Shaker
+        public static void Sacudida(List<int> A)
+        {
+            int N = A.Count;
+            int IZQ = 1;
+            int DER = N - 1;
+            int K = N - 1;
+            int AUX;
+
+            while (DER >= IZQ)
+            {
+                for (int I = DER; I >= IZQ; I--)
+                {
+                    if (A[I - 1] > A[I])
+                    {
+                        AUX = A[I - 1];
+                        A[I - 1] = A[I];
+                        A[I] = AUX;
+                        K = I;
+                    }
+                }
+                IZQ = K + 1;
+
+                for (int I = IZQ; I <= DER; I++)
+                {
+                    if (A[I - 1] > A[I])
+                    {
+                        AUX = A[I - 1];
+                        A[I - 1] = A[I];
+                        A[I] = AUX;
+                        K = I;
+                    }
+                }
+                DER = K - 1;
+            }
+        }
 
         // Insertion
+        public static void InsertionSort(List<int> arr)
+        {
+            int size = arr.Count();
+            for (int i = 1; i < size; i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while(j > -1 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                    j--;
+                }
+            }
+        }
 
         // Selection
 
         // Shell
 
         // Quick Sort
+        public static void QuickSort(List<int> arr)
+        {
+            QuickSort(arr, 0, arr.Count - 1);
+        }
 
         // Heap Sort
+
+        // Private Methods
+        private static void QuickSort(List<int> arr, int leftIndex, int rightIndex)
+        {
+            if (leftIndex >= rightIndex) return;
+
+            int pivotIndex = Pivot(arr, leftIndex, rightIndex);
+            QuickSort(arr, leftIndex, pivotIndex - 1);
+            QuickSort(arr, pivotIndex + 1, rightIndex);
+        }
+
+        private static int Pivot(List<int> arr, int pivotIndex, int endIndex)
+        {
+            int swapIndex = pivotIndex;
+            for (int i = pivotIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i] > arr[pivotIndex])
+                {
+                    swapIndex++;
+                    Swap(arr, swapIndex, i);
+                }
+            }
+            Swap(arr, pivotIndex, swapIndex);
+            return swapIndex;
+        }
+
+        private static void Swap(List<int> arr, int firstIndex, int SecondIndex)
+        {
+            int temp = arr[firstIndex];
+            arr[firstIndex] = arr[SecondIndex];
+            arr[SecondIndex] = temp;
+        }
     }
 }
